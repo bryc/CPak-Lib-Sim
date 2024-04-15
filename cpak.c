a774f9a090ebe456d62853d8b5d4574aae5b7009
bryc <1408749+bryc@users.noreply.github.com>
2024-04-15 18:10:22 -0400

    Hex values where appropriate

--- cpak.c
+++ cpak.c
@@ -6,6 +6,7 @@ uint8_t SRAM[32768] = {0};
 
 // IndexTable parser flags
 //   When checking IndexTable, bit flags are used for each index:
+//
 //   0x01 = this index is a startIndex in NoteTable  (we expect both)
 //   0x02 = this index is a startIndex in IndexTable (we expect both)
 //   0x04 = this index is referenced in IndexTable
@@ -23,7 +24,7 @@ int check() {
         uint8_t si = SRAM[i + 7];
         if(si >= 5 && si <= 127) {
             // set ntblStart flag
-            indexFlags[si] |= 1;
+            indexFlags[si] |= 0x01;
             printf("%02X ", si);
         }
     }
@@ -36,18 +37,18 @@ int check() {
         // values under 5 must also be marked, except 1
         if(ni != 1 && ni < 5) ni = (i - 0x100) / 2;
         // set isDupe flag if a duplicate is found
-        if(indexFlags[ni] & 4) indexFlags[ni] |= 8;
+        if(indexFlags[ni] & 0x04) indexFlags[ni] |= 0x08;
         // set isFound flag.
-        indexFlags[ni] |= 4;
+        indexFlags[ni] |= 0x04;
     }
 
     // Mark all startIndexes found
     printf("iTable: ");
     for(int i = 5; i < 128; i++) {
         // any unreferenced indexes must be startIndexes
-        if((indexFlags[i] & 4) == 0) {
+        if((indexFlags[i] & 0x04) == 0) {
             // set itblStart flag
-            indexFlags[i] |= 2;
+            indexFlags[i] |= 0x02;
             printf("%02X ", i);
         }
     }
@@ -55,14 +56,14 @@ int check() {
     printf("\nParity: ");
     // Check startIndexes
     for(int i = 5; i < 128; i++) {
-        if(indexFlags[i] & 3) printf("%02X ", indexFlags[i] & 3);
+        if(indexFlags[i] & 0x03) printf("%02X ", indexFlags[i] & 0x03);
     }
     printf("\n");
 
     // Validate any index chains found
     for(int i = 5; i < 128; i++) {
         // Only check startIndex if seen in both sources
-        if((indexFlags[i] & 3) == 3) {
+        if((indexFlags[i] & 0x03) == 0x03) {
             uint8_t ci = i, ni, valid = 0, count = 0;
             printf("\n -> Chain @ %02X:\n", ci);
             while(1) {
@@ -70,7 +71,7 @@ int check() {
                 count++;
                 ni = SRAM[0x100 + (ci*2) + 1];
                 // If the nextIndex is a dupe in our flags, the chain is corrupt/unreliable
-                if(ni != 1 && indexFlags[ni] & 8) {
+                if(ni != 1 && indexFlags[ni] & 0x08) {
                     printf("\n    \x1b[31mERROR: Encountered duplicate index %02X\x1b[0m\n", ni);
                     break; // kill the loop
                 }
@@ -86,7 +87,7 @@ int check() {
                 // Mark members of valid chain as 'safe' so they don't get removed.
                 ci = i;
                 while(1) {
-                    indexFlags[ci] |= 16;
+                    indexFlags[ci] |= 0x10;
                     ni = SRAM[0x100 + (ci*2) + 1];
                     if(ni == 1) break;
                     ci = ni;
@@ -98,20 +99,20 @@ int check() {
     // Rebuild IndexTable (clear any invalid data)
     for(int i = 0x100; i < 0x200; i += 2) {
         // Reset any slot that wasn't validated
-        if(i >= 0x10A && (indexFlags[ (i - 0x100) / 2 ] & 16) == 0) {
+        if(i >= 0x10A && (indexFlags[ (i - 0x100) / 2 ] & 0x10) == 0) {
             SRAM[i + 1] = 0x03;
         }
         // These bytes should be zero on a standard C-Pak
         // This can actually repair errors that libultra can't
-        if(SRAM[i + 0] != 0) SRAM[i + 0] = 0x00;
+        if(SRAM[i + 0] != 0x00) SRAM[i + 0] = 0x00;
     }
     // Clean potential garbage data in unused area
     // These bytes were undefined, but garbage data can exist here
     // We should sanitize these to zero to avoid confusion
-    if(SRAM[0x103] != 0) SRAM[0x103] = 0x00; // page 1
-    if(SRAM[0x105] != 0) SRAM[0x105] = 0x00; // page 2
-    if(SRAM[0x107] != 0) SRAM[0x107] = 0x00; // page 3
-    if(SRAM[0x109] != 0) SRAM[0x109] = 0x00; // page 4
+    if(SRAM[0x103] != 0x00) SRAM[0x103] = 0x00; // page 1
+    if(SRAM[0x105] != 0x00) SRAM[0x105] = 0x00; // page 2
+    if(SRAM[0x107] != 0x00) SRAM[0x107] = 0x00; // page 3
+    if(SRAM[0x109] != 0x00) SRAM[0x109] = 0x00; // page 4
 }
 
 int main() {
