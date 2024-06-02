8dd155927078bec4372693bd6b0d5218b64d9784
bryc <1408749+bryc@users.noreply.github.com>
2024-06-02 13:26:10 -0400

    Update cpak.c
    restructured and fixes a bunch of things

--- cpak.c
+++ cpak.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <string.h>
 
-// C-Pak SRAM is 32 KiB
-uint8_t SRAM[32768] = {0};
+typedef unsigned char  u8;
+typedef unsigned short u16;
 
 /*
 IndexTable parser flags
@@ -17,146 +17,90 @@ When checking IndexTable, bit flags are used for each index:
   0x40 = IndexTable 1, Referenced
   0x80 = IndexTable 2, Referenced
 */
-uint8_t iFlags[128] = {0};
+u8 SRAM[32768] = {0}, iFlags[128] = {0}, nTable[16] = {0}, nTotal = 0, ends = 0;
 
-uint8_t repair() {
-    uint8_t si_total = 0, ends = 0;
-    // Count the end markers
-    for(uint8_t i = 5; i < 128; i++) {
-        if(SRAM[0x101 + i*2] == 0x01) ends++;
-    }
-    // Check all startIndexes in NoteTable
-    // First pass - find any conflicts
-    for(uint8_t i = 0; i < 16; i++) {
-        // Read startIndex value from each NoteEntry
-        uint8_t si = SRAM[0x307 + i*32];
-        // If present & valid, attempt to traverse list
-        if(si >= 0x05 && si <= 0x7F) {
-            si_total++;
-            uint8_t ci = si, ni;
-            // primary
-            while(ci != 1) {
-                // Get nextIndex value from Primary iTable
-                ni = SRAM[0x101 + ci*2];
-                // Reached a valid end point
-                if(ni == 1) break;
-                // Error: Value outside range 
-                if(ni > 0x7F || ni < 0x05) break;
-                // Error: Reverse order, Mostly impossible
-                if(ni == ci || ni < ci) break;
-                if(iFlags[ci] & 0x040) {
-                    iFlags[ci] |= 0x04;
-                }
-                iFlags[ci] |= 0x40;
-                // Move nextIndex to currentIndex
-                ci = ni;
-            }
-            // backup
-            while(ci != 1) {
-                // Get nextIndex value from Primary iTable
-                ni = SRAM[0x101 + ci*2];
-                // Reached a valid end point
-                if(ni == 1) break;
-                // Error: Value outside range 
-                if(ni > 0x7F || ni < 0x05) break;
-                // Error: Reverse order, Mostly impossible
-                if(ni == ci || ni < ci) break;
-                if(iFlags[ci] & 0x080) {
-                    iFlags[ci] |= 0x8;
-                }
-                iFlags[ci] |= 0x80;
-                // Move nextIndex to currentIndex
-                ci = ni;
+u8 readChain(u8 si, u8 id, u8 mode) {
+    u16 adrA = id == 1 ? 0x101 : 0x201, adrB = id == 1 ? 0x201 : 0x101;
+    for(u8 ci = si, ni, cnt = 1; ci != 1; ci = ni, cnt++) {
+        ni = SRAM[adrA + (ci << 1)];
+        if(mode == 1 && id == 1 && ni == 1 &&
+          (ends != nTotal && SRAM[adrB + (ci << 1)] != 1) &&
+          (cnt == 1 || SRAM[adrA + (si << 1)] == SRAM[adrB + (si << 1)])) {
+            return 0;
+        }
+        if(ni == 1) return 1;
+        if(ni <= ci || ni > 0x7F || ni < 0x05) return 0;
+        // if(ni != ci+1 && SRAM[adrA-1 + (ci << 1)] && ni != SRAM[adrB + (ci << 1)]) return 0;
+        if(mode == 0) {
+            if(iFlags[ni] & (id << 6)) iFlags[ni] |= (id << 2);
+            iFlags[ni] |= (id << 6);
+        } else if(mode == 1) {
+            if( (ni != ci+1 && ni != SRAM[adrB + (ci << 1)]) ) {
+            if(iFlags[ni] & (id << 2)) return 0;
             }
         }
     }
-    // Check all startIndexes in NoteTable
-    // Second pass - complete check
-    for(uint8_t i = 0; i < 16; i++) {
-        // Read startIndex value from each NoteEntry
-        uint8_t si = SRAM[0x307 + i*32], count = 0;
-        // If present & valid, attempt to traverse list
-        if(si >= 0x05 && si <= 0x7F) {
-            uint8_t ci = si, ni, valid = 0;
-            while(ci != 1) {
-                count++;
-                // Get nextIndex value from Primary iTable
-                ni = SRAM[0x101 + ci*2];
-                // Reached a valid end point
-                if(ni == 1) {
-                    // Detect truncation errors
-                    // If either length = 1 or secondIndex matches... 
-                    // ...check backup ni + total ends found
-                    if( (count == 1 || SRAM[0x101 + si*2] == SRAM[0x201 + si*2]) &&
-                        (ends != si_total && SRAM[0x201 + ci*2] != 1) ) {
-                        break;
-                    }
-                    valid = 1;
-                    break;
-                }
-                // Error: Value outside range 
-                if(ni > 0x7F || ni < 0x05) break;
-                // Error: Reverse order, Mostly impossible
-                if(ni == ci || ni < ci) break;
-                if(iFlags[ci] & 0x04) break;
-                // Move nextIndex to currentIndex
-                ci = ni;
-            }
-            // Valid chain found in Primary iTable
-            // Let's mark them as safe!
-            if(valid) {
-                // Reset currentIndex to the start
-                ci = si;
-                // Output sequence to screen
-                printf("p[ ");
-                // Repeat the loop again
-                while(ci != 1) {
-                    ni = SRAM[0x101 + ci*2];
-                    // Mark secured indexes (Primary)
-                    iFlags[ci] |= 1;
-                    printf("%02X ", ci);
-                    ci = ni;
-                }
-                printf("]\n");
-            // Check backup if valid chain wasn't found
-            } else {
-                // Same loop as before
-                ci = si;
-                while(ci != 1) {
-                    // This time, target the backup
-                    ni = SRAM[0x201 + ci*2];
-                    if(ni == 1) {valid = 1; break;}
-                    if(ni > 0x7F || ni < 0x05) break;
-                    if(ni == ci || ni < ci) break;
-                    if(iFlags[ci] & 0x08) break;
-                    ci = ni;
-                }
-                // Valid chain found in Backup iTable
-                // Let's mark them as safe!
-                if(valid) {
-                    ci = si;
-                    printf("B[ ");
-                    while(ci != 1) {
-                        // Target backup location
-                        ni = SRAM[0x201 + ci*2];
-                        // Mark secured indexes (Backup)
-                        iFlags[ci] |= 2;
-                        printf("%02X ", ci);
-                        ci = ni;
-                    }
-                    printf("]\n");
-                }
-            }
+}
+
+u8 check() {
+    // Count endpoints (values of 1) in Primary iTable
+    for(u8 i = 0; i < 246; i += 2) if(SRAM[0x10b+i] == 1) ends++;
+
+    // Extract valid start indexes from NoteTable
+    for(u8 i = 0, gid, pid, sum, sih, si; i < 16; i++) {
+        si  = SRAM[0x307 + i*32], sih = SRAM[0x306 + i*32];
+        sum = SRAM[0x30A + i*32] | SRAM[0x30B + i*32];
+        pid = SRAM[0x304 + i*32] | SRAM[0x305 + i*32];
+        gid = SRAM[0x300 + i*32] | SRAM[0x301 + i*32] | 
+              SRAM[0x302 + i*32] | SRAM[0x303 + i*32];
+        if(!sih && !sum && gid && pid && si < 128 && si > 4) {
+            //iFlags[si] |= 0x80;
+            nTable[i] = si;
+            nTotal++;
         }
     }
+
+    // IndexTable (Pass 1): Check for duplicates
+    for(u8 i = 0; nTable[i] && i < 16; i++) {
+        readChain(nTable[i], 1, 0); // Primary
+        readChain(nTable[i], 2, 0); // Backup
+    }
+
+    // IndexTable (Pass 2): Validate notes
+    for(u8 i = 0, ci, ni, si; nTable[i] && i < 16; i++) {
+        si = nTable[i];
+        // First check Primary iTable
+        if(readChain(si, 1, 1)) {
+            printf("p[ ");
+            for(ci = si; ci != 1; ci = ni) {
+                ni = SRAM[0x101 + (ci << 1)];
+                iFlags[ci] |= 0x01; // Secure indexes (Primary)
+                printf("%02X ", ci);
+            }
+            printf("]\n");
+        // If unsuccessful, check Backup iTable
+        } else if(readChain(si, 2, 1)) {
+            printf("B[ ");
+            for(ci = si; ci != 1; ci = ni) {
+                ni = SRAM[0x201 + (ci << 1)];
+                iFlags[ci] |= 0x02; // Secure indexes (Backup)
+                printf("%02X ", ci);
+            }
+            printf("]\n");
+        // Note cannot be validated from either Primary or Backup :(
+        } else {
+            printf("CRITICAL ERROR: Note %02X is cannot be recovered.\n", si);
+        }
+    }
+
     // Rebuild IndexTable (clear any invalid data)
-    for(uint8_t i = 0; i < 128; i++) {
+    for(u8 i = 0; i < 128; i++) {
         // If backup valid and primary not, restore backup
-        if(i >= 5 && !(iFlags[i]&1) && iFlags[i]&2) {
+        if(i >= 5 && !(iFlags[i]& 0x01) && iFlags[i] & 0x02) {
             SRAM[0x101 + i*2] = SRAM[0x201 + i*2];
         }
         // Reset any invalid slot
-        else if(i >= 5 && !(iFlags[i]&1)) {
+        else if(i >= 5 && !(iFlags[i] & 0x01)) {
             SRAM[0x101 + i*2] = 0x03;
         }
         // These bytes will be 0 on official C-Pak
@@ -170,32 +114,29 @@ uint8_t repair() {
     if(SRAM[0x107]) SRAM[0x107] = 0x00; // pg 3
     if(SRAM[0x109]) SRAM[0x109] = 0x00; // pg 4
     // Update checksum
-    uint8_t sum8 = 0;
-    for(uint8_t i = 1; i < 128; i++) {
+    u8 sum8 = 0;
+    for(u8 i = 1; i < 128; i++) {
         sum8 += SRAM[0x101 + i*2];
     }
     SRAM[0x101] = sum8;
     // Copy Primary -> Backup
-    for(uint8_t i = 0;; i++) {
+    for(u8 i = 0;; i++) {
         SRAM[0x201 + i*2] = SRAM[0x101 + i*2];
         if(i == 255) break;
     }
 }
 
-int main() {
+int main(int argc, char * argv[]) {
+    if(!argv[1]) {printf("ERROR: A file must be supplied.\n"); return 0;}
     // Load C-Pak file into SRAM state
-    FILE* file = fopen("SimCity 2000 (Japan).pak", "rb");
-    // Note: SimCity 2000 silently repairs C-Pak during boot
-    // which is what I'm using to verify correct behaviors
-    fread(SRAM, sizeof(SRAM), 1, file);
-    
-    // Perform a repair
-    repair();
-  
-    // Display hex dump of IndexTable   
-    for(int i = 0x100; i < 0x200; i++) {
-        if((i & 15) == 0) printf("\n");
-        printf("%02X ", SRAM[i] & 0xFF);
+    FILE* fp = fopen(argv[1], "rb");
+    if(!fp) {printf("ERROR: File not found.\n"); fclose(fp); return 0;}
+    // Detect and skip DexDrive header (via .n64 extension)
+    if(!strcmp(&argv[1][strlen(argv[1]) - 2], "64")) {
+        if(fseek(fp, 0x1040, SEEK_SET) != 0) {fclose(fp); return 0;}
     }
-    return 0;
+    fread(SRAM, sizeof(SRAM), 1, fp); fclose(fp);
+
+    printf("-------- [ %s ] --------\n", argv[1]);
+    check();
 }
